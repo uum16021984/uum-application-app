20a40e02a714cb49716a64ef49b09f5394d09a61
uum16021984 <uum16021984@gmail.com>
2026-06-08 09:40:40 +0800

    Update api.js

--- public/api.js
+++ public/api.js
@@ -1,12 +1,12 @@
 const TOKEN_KEY = 'uum_jwt';
 
 function getToken() {
-  return localStorage.getItem(TOKEN_KEY);
+  return sessionStorage.getItem(TOKEN_KEY);
 }
 
 function setToken(token) {
-  if (token) localStorage.setItem(TOKEN_KEY, token);
-  else localStorage.removeItem(TOKEN_KEY);
+  if (token) sessionStorage.setItem(TOKEN_KEY, token);
+  else sessionStorage.removeItem(TOKEN_KEY);
 }
 
 async function api(path, options = {}) {
